134c3e73fad740edea2a8164d1839daa1dd197ab
yiyen1030 <jackson1030@gmail.com>
2026-06-08 13:09:54 +0800

    更換地圖

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ function initMap() {
     // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //     attribution: '© OpenStreetMap contributors'
     // }).addTo(map);
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_matter/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors © CARTO'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        attribution: '© Stadia Maps © OpenMapTiles © OpenStreetMap contributors'
     }).addTo(map);
 
     markersGroup = L.layerGroup().addTo(map);
